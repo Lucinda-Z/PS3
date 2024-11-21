e4dfa1adbfd15ce16a8a63efabc8c8bebd73bca7
Lucinda-Z <yanyang.zhao28@gmail.com>
2024-11-21 21:38:47 +0000

    Q1 added

--- ps3/data/_sample_split.py
+++ ps3/data/_sample_split.py
@@ -1,7 +1,5 @@
 import hashlib
 
-import numpy as np
-
 # TODO: Write a function which creates a sample split based in some id_column and training_frac.
 # Optional: If the dtype of id_column is a string, we can use hashlib to get an integer representation.
 def create_sample_split(df, id_column, training_frac=0.8):
@@ -12,7 +10,7 @@ def create_sample_split(df, id_column, training_frac=0.8):
     df : pd.DataFrame
         Training data
     id_column : str
-        Name of ID column
+        Name of ID column4
     training_frac : float, optional
         Fraction to use for training, by default 0.9
 
@@ -21,5 +19,13 @@ def create_sample_split(df, id_column, training_frac=0.8):
     pd.DataFrame
         Training data with sample column containing train/test split based on IDs.
     """
+    def hash_to_split(id_value):
+         # Create a hash from the ID
+        hash_value = int(hashlib.md5(str(id_value).encode()).hexdigest(), 16)
+        # Map hash to train/test based on training_frac
+        return 'train' if hash_value % 100 < training_frac * 100 else 'test'
+    
+    # Apply the hash function to create the 'sample' column
+    df['sample'] = df[id_column].apply(hash_to_split)
 
     return df
